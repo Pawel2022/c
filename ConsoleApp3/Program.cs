e3f41cad34ce6fbacc39690ae2a13907c9333040
Pawel2022 <115695008+Pawel2022@users.noreply.github.com>
2023-03-26 12:26:08 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            bool q = true;
+            if (x <= 1)
+            {
+                q = false;
+            }
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    q = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"Jest liczba pierwsza");
+            Console.ReadKey();
+
+        }
+
+    }
+}
